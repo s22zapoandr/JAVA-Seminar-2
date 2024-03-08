3891fdb09fb37a505aeb90c3d3e37e9bf72f5f27
Jēkabs Konošonoks <jacob.konosx@gmail.com>
2024-03-08 16:12:55 +0200

    Modified main fail

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -4,34 +4,137 @@ import model.Grade;
 import model.Professor;
 import model.Student;
 import model.Course;
-public class MainService {
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
+
+public class MainService {
+	private static ArrayList<Professor>AllofProfessors = new
+			ArrayList<Professor>();
+	private static ArrayList<Student>AllofStudents = new
+			ArrayList<Student>();
+	private static ArrayList<Course>AllofCourses = new
+			ArrayList<Course>();
+	private static ArrayList<Grade>AllofGrades = new
+			ArrayList<Grade>();
+	
+//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
 	public static void main(String[] args) {
 		Professor pr1 = new Professor();
-		System.out.println(pr1);
 		Professor pr2 = new Professor("Karina", "Skirmane", Degree.mg);
-		System.out.println(pr2);
 		Professor pr3 = new Professor("Estere", "Vitola", Degree.mg);
-		System.out.println(pr3);
+		AllofProfessors.addAll(Arrays.asList(pr1, pr2, pr3));
 		Student st1 = new Student();
-		System.out.println(st1);
 		Student st2 = new Student("Igors" , "Goncarovs");
-		System.out.println(st2);
 		Student st3 = new Student("Anton" , "Volkov");
-		System.out.println(st3);
-		Course course1= new Course();
-		System.out.println(course1);
-		Course course2= new Course("JAVA", 4, pr2);
-		System.out.println(course2);
-		Course course3= new Course("Data Structures" , 4, pr3);
-		System.out.println(course3);
+		AllofStudents.addAll(Arrays.asList(st1, st2, st3));
+		Course c1= new Course();
+		Course c2= new Course("JAVA", 4, pr2);
+		Course c3= new Course("Data Structures" , 4, pr3);
+		AllofCourses.addAll(Arrays.asList(c1, c2, c3));
 		Grade g1 = new Grade();
-		System.out.println(g1);
-		Grade g2 = new Grade(2, st2, course2);
-		System.out.println(g2);
-		Grade g3 = new Grade(10, st3, course3);
-		System.out.println(g3);
+		Grade g2 = new Grade(2, st2, c2);
+		Grade g3 = new Grade(10, st3, c3);
+		AllofGrades.addAll(Arrays.asList(g1, g2, g3));
+		
+		
+		for(Professor tempProf : AllofProfessors) {
+			System.out.println(tempProf);
+		}
+		for(Student tempSt : AllofStudents) {
+			System.out.println(tempSt);
+		}
+		for(Course tempC : AllofCourses) {
+			System.out.println(tempC);
+		}
+		for(Grade tempG : AllofGrades) {
+			System.out.println(tempG);
+		}
+		
+		try {
+			System.out.println("Average grade for " + st1.getName() + " " + st1.getSurname() + " " + calculateAVG(st1) );
+			System.out.println("Average weighted grade for " + st2.getName() + " " + st2.getSurname() + " " + calculateAVWG(st2) );
+			System.out.println("Average grade for " + c1.getTitle() + ": " + courseAVG(c1));
+			for()
+			
+		}
+		
+	
+	}
+		public static float calculateAVG(Student inputStudent) throws Exception{
+			if(inputStudent == null) throw new Exception("Problems with input");
+			
+			float sum = 0;
+			int howMany = 0;
+			
+			for(Grade tempG : AllofGrades) {
+				if(tempG.getStudent().equals(inputStudent)) {
+					sum += tempG.getValue();
+					howMany++;
+				}
+			}
+			return sum/howMany;
+		}
+		
+		public static float calculateAVWG(Student inputStudent) throws Exception{
+			if(inputStudent == null) throw new Exception("Problems with input");
+			
+			float sum = 0;
+			float howManyCP = 0;
+			
+			for(Grade tempG : AllofGrades) {
+				if(tempG.getStudent().equals(inputStudent)) {
+					sum += tempG.getValue() * tempG.getCourse().getCreditpoints();
+					howManyCP += tempG.getCourse().getCreditpoints();
+				}
+			}
+			return sum/howManyCP;
+		}
+		public static float courseAVG(Course inputCourse) throws Exception{
+			if(inputCourse == null) throw new Exception("Skill isuues");
+			 
+			float sum = 0;
+			int howManyStudents = 0;
+			
+			for(Grade tempG : AllofGrades) {
+				if(tempG.getCourse().equals(inputCourse)) {
+					sum += tempG.getValue();
+					howManyStudents++;
+				}
+			}
+			return sum/howManyStudents;
+		}
+		public static int ProfCourseCount(Professor inputProfessor) throws Exception{
+			if(inputProfessor == null) throw new Exception("Skill issues");
+			
+			int sum = 0;
+			for(Course tempC : AllofCourses) {
+				if(tempC.getProfessor().equals(inputProfessor)) {
+					sum++;
+				}
+			}
+			return sum;
+		}
+		
+		public static void sortStudents() throws Exception{
+			for(int i = 0; i < AllofStudents.size(); i++) {
+				for(int j = 0; j < AllofStudents.size(); j++) {
+					Student tempI = AllofStudents.get(i);
+					Student tempJ = AllofStudents.get(j);
+					if (tempI > tempJ) {
+						
+					}
+				}
+			}
+		}
+		
+		
+		
 		
 	}
 
-}
+
