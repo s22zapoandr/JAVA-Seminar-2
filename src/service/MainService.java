d4e320d24b6fd8f24efe43eaf396f44d55c5bbec
Jēkabs Konošonoks <jacob.konosx@gmail.com>
2024-03-22 15:05:07 +0200

    Added polymorphism

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -8,42 +8,90 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 import model.Course;
 import model.Degree;
 import model.Grade;
+import model.Person;
 import model.Professor;
 import model.Student;
 //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 
 public class MainService {
 
 	
-	private static ArrayList<Professor> allProfessors = new ArrayList<Professor>();
-	private static ArrayList<Student> allStudents = new ArrayList<Student>();
+
 	private static ArrayList<Course> allCourses = new ArrayList<Course>();
 	private static ArrayList<Grade> allGrades= new ArrayList<Grade>();
+	private static ArrayList<Person> allPersons= new ArrayList<Person>();
+	
+	
+	/*
+	public static boolean isStudent(t
+) {
+		for(Person tempPer :allPersons2) {
+			if(tempPer instanceof Student) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
+		
+	}
+	/*
+	public static void isStudent(ArrayList<Person> allPersons2) throws Exception{
+		for(Person tempPer :allPersons2) {
+			if(tempPer instanceof Student) {
+				Student tempSt  =(Student)tempPer;
+				System.out.println(tempSt);
+			}
+			else {
+				throw new Exception ("No students in array");
+			}
+		}
+		
+	}
+	
+	public static void isProfessor(Person [] allPersons) throws Exception{
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Professor) {
+				Professor tempPr  =(Professor)tempPer;
+				System.out.println(tempPr);
+			}
+			else {
+				throw new Exception ("No professors in array");
+			}
+		}
+		
+	}
+	
+	
+	public static boolean isProfessor(Person [] allPersons) {
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Professor) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
+		
+	}*/
 	
 	public static void main(String[] args) {
 		Professor pr1 = new Professor();//John Big - default Professor
 		Professor pr2 = new Professor("Karina", "Skirmante","312311-23126", Degree.mg);
 		Professor pr3 = new Professor("Estere", "Vitola",  "123467-09853", Degree.mg);
 		Professor pr4 = new Professor("Marcis", "Naktins","213174-86431", Degree.mg);
-		allProfessors.addAll(Arrays.asList(pr1, pr2, pr3, pr4));
-		//TODO take a look to ArrayList class in JAVA documentations
-		for(int i = 0; i < allProfessors.size(); i++) {
-			System.out.println(allProfessors.get(i));
-		}
-		System.out.println("------------------------------");
-		for(Professor tempPr : allProfessors) {
-			System.out.println(tempPr);
-		}
-		System.out.println("------------------------------");
 		
-		
-		
+
+
 		Student st1 = new Student();//Lara Bernardes student
 		Student st2 = new Student("Viktors", "Kokin", "123456-09876");
 		Student st3 = new Student("Davyd", "Akimov", "121298-67894");
-		allStudents.addAll(Arrays.asList(st1,st2,st3));
 		
-		for(Student tempSt: allStudents) {
-			System.out.println(tempSt);
+		allPersons.addAll(Arrays.asList(pr1, pr2, pr3, pr4, st1, st2, st3));
+		
+		for(Person tempPer : allPersons) {
+			System.out.println(tempPer);
 		}
 		
 		
@@ -96,44 +144,54 @@ public class MainService {
 					calculateHowManyCoursesbyProfessor(pr3) + " courses");
 			
 			System.out.println("------------------------");
-			for(Student tempSt: allStudents) {
-				System.out.println(tempSt.getName() + " " + tempSt.getSurname() + " -> "
-						+calculateAVGForStudent(tempSt));
-			}
-			
 			sortStudents();
 			System.out.println("------------------------");
-			for(Student tempSt: allStudents) {
-				System.out.println(tempSt.getName() + " " + tempSt.getSurname() + " -> "
+			for(Person tempPer :allPersons) {
+				if(tempPer instanceof Student) {
+					Student tempSt  =(Student)tempPer;
+					System.out.println(tempSt.getName() + " " + tempSt.getSurname() + " -> "
 						+calculateAVGForStudent(tempSt));
+				}
 			}
 			
-			Student temp = retrieveStudentByPersoncode("123456-09876");
+			Person temp = retrieveStudentByPersoncode("123456-09876");
 			System.out.println("Retrieve student by person code " + temp);
 			
 			createStudent("Sarah", "Green", "148336-87771");
 			
 			System.out.println("Create student testing (Sarah) - ");
-			for(Student tempSt : allStudents) {
-				System.out.println(tempSt);
+			for(Person tempPer :allPersons) {
+				if(tempPer instanceof Student) {
+					Student tempSt  =(Student)tempPer;
+					System.out.println(tempSt.getName() + " " + tempSt.getSurname() );
+				}
 			}
 			
 			updateStudentbyPersonCode("Viktors", "Kokin",  "123456-09876");
 			System.out.println("Update student testing Viktors - ");
-			for (Student tempSt : allStudents) {
-				System.out.println(tempSt);
+			for(Person tempPer :allPersons) {
+				if(tempPer instanceof Student) {
+					Student tempSt  =(Student)tempPer;
+					System.out.println(tempSt.getName() + " " + tempSt.getSurname() );
+				}
 			}
 			
 			deleteStudentByPersonCode("121298-67894");
 			System.out.println("Delete student testing (David)");
-			for (Student tempSt : allStudents) {
-				System.out.println(tempSt);
+			for(Person tempPer :allPersons) {
+				if(tempPer instanceof Student) {
+					Student tempSt  =(Student)tempPer;
+					System.out.println(tempSt.getName() + " " + tempSt.getSurname() );
+				}
 			}
 			
 			deleteStudentByPersonCode("12431241224");
 			System.out.println("Delete student testing (invalid)");
-			for (Student tempSt : allStudents) {
-				System.out.println(tempSt);
+			for(Person tempPer :allPersons) {
+				if(tempPer instanceof Student) {
+					Student tempSt  =(Student)tempPer;
+					System.out.println(tempSt.getName() + " " + tempSt.getSurname() );
+				}
 			}
 			
 		}
@@ -146,7 +204,7 @@ public class MainService {
 
 	
 
-	public static float calculateAVGForStudent(Student inputStudent) throws Exception {
+	public static float calculateAVGForStudent(Person inputStudent) throws Exception {
 		if(inputStudent == null) throw new Exception("Problems with input");
 		
 		
@@ -169,7 +227,7 @@ public class MainService {
 	//JAVA 6 and DataStructure 8 AVG = (6+8)/2 = 7
 	//JAVA 6 (4CP) DataStructure 8 (2CP) = ((6 * 4CP) + (8 * 2CP))/ (4CP +2CP) = (24 +16)/6= 40/6 = 6.666
 		
-	public static float calculateWeightedAVGForStudent(Student inputStudent) throws Exception {
+	public static float calculateWeightedAVGForStudent(Person inputStudent) throws Exception {
 		if(inputStudent == null) throw new Exception("Problems with input");
 		
 		float sum = 0;
@@ -225,90 +283,96 @@ public class MainService {
 	
 	public static void sortStudents() throws Exception {
 		
-		
-		for(int i = 0; i < allStudents.size(); i++) {
-			for(int j = 0; j < allStudents.size(); j++) {
-				
-				Student tempI = allStudents.get(i);
-				Student tempJ = allStudents.get(j);
-				if(calculateAVGForStudent(tempJ) < calculateAVGForStudent(tempI)) {
-					Student temp  =  allStudents.get(i);
-					allStudents.set(i, allStudents.get(j));
-					allStudents.set(j, temp);
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Student) {
+				for(int i = 0; i < allPersons.size(); i++) {
+					for(int j = 0; j < 	allPersons.size(); j++) {
+						Person tempI = allPersons.get(i);
+						Person tempJ = allPersons.get(j);
+						if(calculateAVGForStudent(tempJ) < calculateAVGForStudent(tempI)) {
+							Person temp  =  allPersons.get(i);
+							allPersons.set(i, allPersons.get(j));
+							allPersons.set(j, temp);
+						}
+					}
 				}
-				
-				
 			}
 		}
 	}
-	
 	//TODO
 	//calculates how many professors have phd as degree
 	
 	public static int howManyProfessorsHavePHD() {
 		
 		int howMany = 0;
-		
-		for(Professor tempPr: allProfessors) {
-			if(tempPr.getProfDegree().equals(Degree.phd)) {
-				howMany++;
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Professor) {
+				Professor tempPr  =(Professor)tempPer;
+				if(tempPr.getProfDegree().equals(Degree.phd)) {
+					howMany++;
 			}
 		}
-		
+	}
 		return howMany;
 		
 	}
 	//calculates how many grades are smaller than 4 in specific course
 	//calculates how many CP professor need to lead
 	
-	public static Student retrieveStudentByPersoncode(String inputPersonCode) throws Exception {
+	public static Person retrieveStudentByPersoncode(String inputPersonCode) throws Exception {
 		if(inputPersonCode == null) throw new Exception ("Invalid input");
 		
-		for(Student tempSt: allStudents) {
-			if(tempSt.getPersonCode().equals(inputPersonCode)) {
-				return tempSt;
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Student) {
+				if(tempPer.getPersonCode().equals(inputPersonCode)) {
+				   return tempPer;
 			}
 		}
-		throw new Exception("Student not found");
 	}
-	 
+		throw new Exception("Student not found");
+	
+}
 	public static void createStudent(String inputName, String inputSurname, String inputPersonCode) throws Exception {
 		if(inputPersonCode == null || inputName == null || inputSurname == null) throw new Exception ("Invalid input");
-		
-		for(Student tempSt: allStudents) {
-			if(tempSt.getPersonCode().equals(inputPersonCode)) throw new Exception ("Student with input person code already exists");
+
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Student) {
+					if(tempPer.getPersonCode().equals(inputPersonCode)) throw new Exception ("Student with input person code already exists");
 			else {
 				Student newStudent = new Student(inputName, inputSurname, inputPersonCode);
-				allStudents.add(newStudent);
+				allPersons.add(newStudent);
 				System.out.println("Meet new student -" + newStudent.toString() );
 			}
 		}
 	}
-	
+}	
 	public static void updateStudentbyPersonCode(String inputName, String inputSurname, String inputPersonCode) throws Exception{
 		if(inputPersonCode == null || inputName == null || inputSurname == null) throw new Exception ("Invalid input");
 		
-		for(Student tempSt: allStudents) {
-			if(tempSt.getPersonCode().equals(inputPersonCode)) {
-				tempSt.setName(inputSurname);
-				tempSt.setSurname(inputSurname);
-				return;
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Student) {
+				Student tempSt  =(Student)tempPer;
+					if(tempSt.getPersonCode().equals(inputPersonCode)) {
+						tempSt.setName(inputSurname);
+						tempSt.setSurname(inputSurname);
+						return;
 			}
 			
 		} throw new Exception("No student found with this person code");
 	
 	}
+	}
 	
 	public static void deleteStudentByPersonCode(String inputPersonCode) throws Exception{
 		if(inputPersonCode == null ) throw new Exception ("Invalid input");
-		
-		for (Student tempSt: allStudents) {
-			if(tempSt.getPersonCode().equals(inputPersonCode)) {
-				allStudents.remove(tempSt);
+		for(Person tempPer :allPersons) {
+			if(tempPer instanceof Student) {
+					if(tempPer.getPersonCode().equals(inputPersonCode)) {
+				allPersons.remove(tempPer);
 				return;
 			}
 		}
 		throw new Exception("No student found with this person code");
 	}
-	
+	}
 }
